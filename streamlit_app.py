b39cae1b2405b955a427554a691f789b5677b469
Dan Becker <dansbecker@gmail.com>
2021-02-11 20:17:44 -0700

    Use real data for scatter on map. But regression isn't line yet

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from streamlit_folium import folium_static
 import folium
 from branca.colormap import linear, LinearColormap
 
-st.write('**Next step: Use the real data**')
 st.write('**Let user select time period**')
 
 hide_menu_style = """
@@ -15,6 +14,19 @@ hide_menu_style = """
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+
+@st.cache
+def get_annual_stats():
+    raw_data = pd.read_parquet('./data/weather_1970_to_2020.parquet')
+    raw_data['date'] = pd.to_datetime(raw_data.date)
+    raw_data['year'] = raw_data.date.dt.year
+    annual_stats = raw_data.fillna(0).groupby(['station_id', 'year']).agg(
+                            {'max_temp_c': ['mean'],
+                            'min_temp_c': ['mean'],
+                            'precip_mm': ['sum']}
+    ).reset_index()
+    annual_stats.columns = ['station_id', 'year', 'max_temp_c', 'min_temp_c', 'precip_mm']
+    return annual_stats
 
 @st.cache
 def read_base_file():
@@ -37,6 +49,10 @@ metric_units = {'slope_max_temp': '° / year',
                 'slope_min_temp': '° / year',
                 'pct_precip_change': '% / year',}
 
+name_in_annual_data = {'slope_max_temp': 'max_temp_c',
+                       'slope_min_temp': 'min_temp_c',
+                       'pct_precip_change': 'precip_mm',}
+
 
 reverse_colormap = {'slope_max_temp': True,
                     'slope_min_temp': True,
@@ -46,7 +62,10 @@ reverse_colormap = {'slope_max_temp': True,
     
 @st.cache(hash_funcs={folium.folium.Map: lambda _: None}, allow_output_mutation=True)
 def make_map(field_to_color_by):
-    main_map = folium.Map()
+    annual_data_field = name_in_annual_data[field_to_color_by]
+    city_plot_width = 200
+    city_plot_height = 200
+    main_map = folium.Map(width=800, height=500, zoom_start=3)
     colormap = linear.RdYlBu_08.scale(station_stats[field_to_color_by].quantile(0.05),
                                       station_stats[field_to_color_by].quantile(0.95))
     if reverse_colormap[field_to_color_by]:
@@ -59,13 +78,13 @@ def make_map(field_to_color_by):
     colormap.caption = metric_desc
     colormap.add_to(main_map)
     for _, city in station_stats.iterrows():
-        sample_data = pd.DataFrame({'x': [1,2,3,4,5,6],
-                                    'y': [5,4,3,4,5,6]})
-        some_chart = alt.Chart(sample_data).mark_line().encode(
-            alt.X('x'),
-            alt.Y('y')
-        ).to_json()
+        annual_this_city = annual_stats.loc[annual_stats.station_id == city.station_id]
+        scatter = alt.Chart(annual_this_city).mark_point().encode(
+            alt.X('year', scale=alt.Scale(zero=False), axis=alt.Axis(format="d")),
+            alt.Y(annual_data_field, scale=alt.Scale(zero=False))
+        )
 
+        full_chart = scatter + scatter.transform_regression('year', annual_data_field).mark_line()
         icon_color = colormap(city[field_to_color_by])
         folium.CircleMarker(location=[city.lat, city.lon],
                     tooltip=f"{city.municipality}\n  value: {city[field_to_color_by]}{metric_unit}",
@@ -74,14 +93,18 @@ def make_map(field_to_color_by):
                     color=None,
                     fill_opacity=0.7,
                     radius=5,
-                    popup = folium.Popup(max_width=400).add_child(
-                                            folium.features.VegaLite(some_chart, width=400, height=300)
+                    popup = folium.Popup().add_child(
+                                            folium.features.VegaLite(full_chart.to_json(),
+                                                                     width=city_plot_width + 50,
+                                                                     height=city_plot_height + 50
+                                                                     )
                                             )
                     ).add_to(main_map)
     return main_map
 
+annual_stats = get_annual_stats()
 station_stats = read_base_file()
-# st.dataframe(station_stats)
+
 st.header('Map')
 
 metric_for_map = st.selectbox('Climate metric for map',
